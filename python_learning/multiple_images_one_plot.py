4f4b2fbc9840283f9f67441cd8ba8c3a99a95bf5
timf34 <farrelti@tcd.ie>
2022-11-29 22:22:51 +0000

    more code/ practice on formatting

--- python_learning/multiple_images_one_plot.py
+++ python_learning/multiple_images_one_plot.py
@@ -24,10 +24,46 @@ def create_image(colour: str, size: int):
 
 
 def plot_images(image1, image2):
-    fig, (ax1, ax2) = plt.subplots(1, 2)
+    # fig, (ax1, ax2) = plt.subplots(1, 2)
+    # ax1.imshow(image1)
+    # ax2.imshow(image2)
+    # plt.show()
+
+    # Now plot the images so they are vertical
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    ax1.imshow(image1)
+    ax2.imshow(image2)
+    # plt.show()
+
+    # # Now enlarge the image plots using figsize
+    # fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
+    # ax1.imshow(image1)
+    # ax2.imshow(image2)
+    # plt.show()
+
+    # Now remove whitespace between the plots
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(5, 5), tight_layout=True)
+    ax1.imshow(image1)
+    ax2.imshow(image2)
+    # plt.show()
+
+    # Now adjust the subplots so they are closer together
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(5, 5), tight_layout=True, gridspec_kw={'hspace': .1})
+    ax1.imshow(image1)
+    ax2.imshow(image2)
+    # plt.show()
+
+    # Now define a GridSpec so we can adjust the height and width of the plots
+    fig = plt.figure(figsize=(5, 5), tight_layout=True)
+    gs = fig.add_gridspec(2, 1, hspace=.1)
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax2 = fig.add_subplot(gs[1, 0])
     ax1.imshow(image1)
     ax2.imshow(image2)
     plt.show()
+
+
+
 
 
 def main():
