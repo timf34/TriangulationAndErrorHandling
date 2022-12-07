b17cefe9aa12176331b6fd838ff8aa7d54587254
timf34 <farrelti@tcd.ie>
2022-12-07 21:48:39 +0000

    working sample saving script for plt plots

---
+++ python_learning/plt_video_saving_practice.py
@@ -0,0 +1,54 @@
+"""
+This file is primarily used to practice saving videos of matplotlib plots.
+
+We will create a function that creates a simple plot given an input... we can then loop over this over time to create
+something we can take a video of.
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
+
+
+def create_plot(x, y):
+    """
+    This function creates a simple plot given an input x and y.
+    """
+    # Create a figure
+    fig = plt.figure()
+    # Create an axis
+    ax = fig.add_subplot(111)
+    # Plot the data
+    ax.plot(x, y)
+    # Return the figure
+    return fig
+
+
+def update(i):
+    """
+    This function updates the plot given an input i.
+    """
+    # Clear the axis
+    ax.clear()
+    # Plot the data
+    ax.plot(x, y)
+    # Set the title
+    ax.set_title(f"i = {i}")
+    # Return the axis
+    return ax
+
+
+# Create some data
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# Create the figure
+fig = create_plot(x, y)
+# Create the axis
+ax = fig.add_subplot(111)
+
+# Create the animation
+ani = FuncAnimation(fig, update, frames=np.arange(0, 10, 0.1), interval=100)
+
+# Save the animation
+ani.save("test.gif")
