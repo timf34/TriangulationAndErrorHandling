527fa01fd25747e5aa3ae9bd6a451026483b523e
timf34 <farrelti@tcd.ie>
2022-12-07 23:23:45 +0000

    func animation isn't working, getting a weird error, going to use opencv instead

--- triangulation_visualization.py
+++ triangulation_visualization.py
@@ -3,8 +3,9 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.animation import PillowWriter
 
-from typing import Tuple, List
+from typing import Tuple, List, Generator
 
 from data.bohs_dataset import create_triangulation_dataset
 from data_classes import Detections
@@ -51,19 +52,26 @@ class TriangulationVisualization:
         assert 0 <= x <= self.pitch_width and 0 <= y <= self.pitch_height, f"x and y must be between {self.pitch_width} and {self.pitch_height}"
         return cv2.circle(self.pitch_image, (x, y), 15, (255, 0, 0), -1)
 
-    def run(self):
+    @staticmethod
+    def x_y_to_detection(x_1, y_1, x_3, y_3, i) -> Tuple[Detections, Detections]:
+        d1 = Detections(camera_id=1, probability=0.9, timestamp=i, x=x_1, y=y_1, z=0)
+        d2 = Detections(camera_id=3, probability=0.9, timestamp=i, x=x_3, y=y_3, z=0)
+        return d1, d2
+
+    @staticmethod
+    def create_plot():
+        fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(5, 5), tight_layout=True,
+                                            gridspec_kw={'hspace': .1})
+        # fig.canvas.draw()
+        return fig, ax1, ax2, ax3
+
+    def get_triangulated_images(self) -> Generator:
         tracker = MultiCameraTracker()
         tracker.add_camera(1, JETSON1_REAL_WORLD)
         tracker.add_camera(3, JETSON3_REAL_WORLD)
 
-        # Create a video write object to save the matplotlib figure to
-        video_writer = cv2.VideoWriter('triangulation_visualization.avi', cv2.VideoWriter_fourcc(*'MJPG'), 10, (600, 600))
-
-        # We will now open the video write object and write the frames to it
-
-
-        for i, (image_1, image_2, box_1, box_2, label_1, label_2, image_path_1, image_path_2) in enumerate(self.dataset):
-
+        for i, (image_1, image_2, box_1, box_2, label_1, label_2, image_path_1, image_path_2) in enumerate(
+                self.dataset):
 
             # Get x and y coordinates of the box
             x_1, y_1 = get_xy_from_box(box_2)
@@ -72,10 +80,8 @@ class TriangulationVisualization:
             # TODO: mirroring for Jetson3 to bring the origins a bit closer together in the diff plances (in my mind at least, haven't tested to see if it works better yet)
             x_3 = 1920 - x_3
 
-            d1 = Detections(camera_id=1, probability=0.9, timestamp=i, x=x_1, y=y_1, z=0)
-            d2 = Detections(camera_id=3, probability=0.9, timestamp=i, x=x_3, y=y_3, z=0)
+            d1, d2 = self.x_y_to_detection(x_1, y_1, x_3, y_3, i)
             det = tracker.multi_camera_analysis(d1, d2)
-
 
             if det is not None:
                 det.x = det.x * (1920 / 102)
@@ -84,6 +90,54 @@ class TriangulationVisualization:
             else:
                 pitch_image = self.draw_point(0, 0)
 
+            yield image_1, image_2, pitch_image
+
+    def run(self):
+        fig, ax1, ax2, ax3 = self.create_plot()
+
+        def update_plot(i) -> None:
+            # Unpack i
+            image_1, image_2, pitch_image = i
+            ax1.plot(image_1)
+            ax2.plot(image_2)
+            ax3.plot(pitch_image)
+
+        # Actually have to preload the frames into a list, as passing a generator to FuncAnimation doesn't work.
+        _frames = []
+        for i in self.get_triangulated_images():
+            _frames.append(i)
+
+        ani = FuncAnimation(fig, update_plot, frames=_frames)
+        # my_writer = PillowWriter(fps=20, codec='libx264', bitrate=2)
+        ani.save("lets_go.gif", writer='pillow', fps=20, codec='libx264', bitrate=2)
+        # plt.show()
+
+    def og_run(self):
+        tracker = MultiCameraTracker()
+        tracker.add_camera(1, JETSON1_REAL_WORLD)
+        tracker.add_camera(3, JETSON3_REAL_WORLD)
+
+        for i, (image_1, image_2, box_1, box_2, label_1, label_2, image_path_1, image_path_2) in enumerate(
+                self.dataset):
+
+            # Get x and y coordinates of the box
+            x_1, y_1 = get_xy_from_box(box_2)
+            x_3, y_3 = get_xy_from_box(box_1)
+
+            # TODO: mirroring for Jetson3 to bring the origins a bit closer together in the diff plances (in my mind at least, haven't tested to see if it works better yet)
+            x_3 = 1920 - x_3
+
+            d1, d2 = self.x_y_to_detection(x_1, y_1, x_3, y_3, i)
+            det = tracker.multi_camera_analysis(d1, d2)
+
+            if det is not None:
+                det.x = det.x * (1920 / 102)
+                det.y = det.y * (1218 / 64)
+                pitch_image = self.draw_point(int(det.x), int(det.y))
+            else:
+                pitch_image = self.draw_point(0, 0)
+
+            # Creating the plot
             if i == 0:
                 fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(5, 5), tight_layout=True,
                                                     gridspec_kw={'hspace': .1})
@@ -94,50 +148,19 @@ class TriangulationVisualization:
                 i3 = ax3.imshow(pitch_image)
                 plt.show(block=False)
             else:
+                # Updating the plot...
                 i1.set_data(image_1)
                 i2.set_data(image_2)
                 i3.set_data(pitch_image)
                 plt.show(block=False)
                 plt.pause(0.000001)
-                # Save the figure to the video writer
-                # video_writer.write(cv2.cvtColor(np.array(fig.canvas.renderer._renderer), cv2.COLOR_RGB2BGR))
-
-
-
-            # self.plot_images(image_1, image_2, pitch_image)
-
-            # print(box_1)
-
-            # Write our plt figure to the video writer
-            # video_writer.write(cv2.cvtColor(pitch_image, cv2.COLOR_RGB2BGR))
-
-            # Clear the figure so we can plot the next frame
-            # plt.clf()
-            # plt.close()
-
-            # Wait for user to press a key before showing next plot
-            # plt.waitforbuttonpress(0)
-            # plt.close()
-
-            # if i == 10:
-            #     break
-
-        # Close the video writer
-        video_writer.release()
 
         # Close the figure
         plt.close()
 
 
-
-
 def main():
     triangulation = TriangulationVisualization()
-
-
-
-
-
     triangulation.run()
 
 
