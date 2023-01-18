eacda7a4cea150d18e22edf37572e41cce1efdfd
timf34 <farrelti@tcd.ie>
2023-01-18 11:22:10 +0000

    added get_image_field_coordinates to config.py

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 
-
+# TODO: note sure what this class is for... Its not used anywhere in the code.
 @dataclass
 class TriangulationConfig:
     image_folders: Tuple[str, str] = (
@@ -16,3 +16,16 @@ class TriangulationConfig:
     only_matching_frames: bool = True  # Only show frames where both cameras have a frame with a ball.
     only_ball_frames: bool = True  # Only show frames where the ball is in the frame.
 
+
+def get_image_field_coordinates() -> Dict[str, Tuple[Tuple[int, int], Tuple[int, int], Tuple[int, int], Tuple[int, int]]]:
+    """
+    This function returns a dict containing the boundaries of the field in the image for each camera.
+    Where the camera ID's are "1" and "3"
+    Coordinates are in the format (x, y), and start from the top left corner and go clockwise.
+    Also note that the top left corner is the origin here (i.e. 0,0)
+    :return: Dict[str, Tuple[int, int, int, int]]
+    """
+    return {
+        "1": ((0, 580), (1918, 576), (1920, 1080), (0, 1080)),
+        "3": ((0, 260), (1920, 230), (1920, 980), (0, 740)),
+    }
